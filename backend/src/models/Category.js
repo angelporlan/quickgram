e7f7e1154ec1a4851eef75d4ecee966aea5bcf79
angelporlan <angeelporlan4@gmail.com>
2025-12-13 09:26:14 +0100

    Add Category model definition with Sequelize

---
+++ backend/src/models/Category.js
@@ -0,0 +1,11 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../config/db.js";
+
+export const Category = sequelize.define("Category", {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    tableName: "categories"
+});
