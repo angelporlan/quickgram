5df5b45cf0bb15332dd37994bc14bacf1ce3315d
angelporlan <angeelporlan4@gmail.com>
2025-12-13 09:26:40 +0100

    Add index file to define model associations for User, Exercise, Subcategory, Level, and Category

---
+++ backend/src/models/index.js
@@ -0,0 +1,61 @@
+import { User } from "./user.js";
+import { UserExerciseAttempt } from "./UserExerciseAttempt.js";
+import { Exercise } from "./Exercise.js";
+import { Subcategory } from "./Subcategory.js";
+import { Level } from "./Level.js";
+import { Category } from "./Category.js";
+
+// User ↔ UserExerciseAttempt
+User.hasMany(UserExerciseAttempt, {
+    foreignKey: "user_id",
+    onDelete: "CASCADE"
+});
+UserExerciseAttempt.belongsTo(User, {
+    foreignKey: "user_id"
+});
+
+// Exercise ↔ UserExerciseAttempt
+Exercise.hasMany(UserExerciseAttempt, {
+    foreignKey: "exercise_id",
+    onDelete: "CASCADE"
+});
+UserExerciseAttempt.belongsTo(Exercise, {
+    foreignKey: "exercise_id"
+});
+
+// Category ↔ Subcategory
+Category.hasMany(Subcategory, {
+    foreignKey: "category_id",
+    onDelete: "CASCADE"
+});
+Subcategory.belongsTo(Category, {
+    foreignKey: "category_id"
+});
+
+// Subcategory ↔ Exercise
+Subcategory.hasMany(Exercise, {
+    foreignKey: "subcategory_id",
+    onDelete: "CASCADE"
+});
+Exercise.belongsTo(Subcategory, {
+    foreignKey: "subcategory_id"
+});
+
+// Level ↔ Exercise
+Level.hasMany(Exercise, {
+    foreignKey: "level_id",
+    onDelete: "CASCADE"
+});
+Exercise.belongsTo(Level, {
+    foreignKey: "level_id"
+});
+
+
+export {
+    User,
+    UserExerciseAttempt,
+    Exercise,
+    Subcategory,
+    Level,
+    Category
+};
