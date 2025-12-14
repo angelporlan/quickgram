4b84d64193621516abe1f1d30f729213a49508b5
angelporlan <angeelporlan4@gmail.com>
2025-12-14 13:12:22 +0100

    Add user routes to server for handling user-related requests

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,6 +7,7 @@ import "dotenv/config";
 import exerciseAttemptRoutes from "./routes/exerciseAttempt.routes.js";
 import authRoutes from "./routes/auth.routes.js";
 import exerciseRoutes from "./routes/exercise.routes.js";
+import userRoutes from "./routes/user.routes.js";
 
 import { seedLevels } from "./seeds/seedLevels.js";
 import { seedCategories } from "./seeds/seedCategories.js";
@@ -26,6 +27,7 @@ app.get("/", (req, res) => {
 app.use("/api", exerciseAttemptRoutes);
 app.use("/api", authRoutes);
 app.use("/api", exerciseRoutes);
+app.use("/api", userRoutes);
 
 (async () => {
     try {
