3658b82b67b84599e4054049f199ce257dc8ba6e
angelporlan <angeelporlan4@gmail.com>
2026-01-04 17:08:25 +0100

    feat: add Lottie animations for category icons and enhance hover effects
    - Introduced Lottie animations for the categories: Grammar, Vocabulary, Reading, Listening, Writing, and Use of English.
    - Updated categories.component.html to replace SVG icons with Lottie animations and added mouse enter/leave events for hover effects.
    - Implemented animation control methods in categories.component.ts to play and stop animations on hover.
    - Added a new JSON file for the Writing animation.
    - Styled sidebar component to highlight goal streaks with an orange color.

--- frontend/src/app/components/categories/categories.component.ts
+++ frontend/src/app/components/categories/categories.component.ts
@@ -1,12 +1,67 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { LottieComponent, AnimationOptions } from 'ngx-lottie';
+import { AnimationItem } from 'lottie-web';
 
 @Component({
   selector: 'app-categories',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [RouterLink, LottieComponent],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.css'
 })
 export class CategoriesComponent {
+  grammarOptions: AnimationOptions = {
+    path: '/svg-lottie/grammar.json',
+    loop: true,
+    autoplay: false
+  };
 
+  vocabularyOptions: AnimationOptions = {
+    path: '/svg-lottie/vocabulary.json',
+    loop: true,
+    autoplay: false
+  };
+
+  readingOptions: AnimationOptions = {
+    path: '/svg-lottie/reading.json',
+    loop: true,
+    autoplay: false
+  };
+
+  listeningOptions: AnimationOptions = {
+    path: '/svg-lottie/listening.json',
+    loop: true,
+    autoplay: false
+  };
+
+  writingOptions: AnimationOptions = {
+    path: '/svg-lottie/writting.json',
+    loop: true,
+    autoplay: false
+  };
+
+  useOfEnglishOptions: AnimationOptions = {
+    path: '/svg-lottie/use-of-english.json',
+    loop: true,
+    autoplay: false
+  };
+
+  private animationItems: { [key: string]: AnimationItem } = {};
+
+  onAnimationCreated(animationItem: AnimationItem, category: string): void {
+    this.animationItems[category] = animationItem;
+  }
+
+  onHover(category: string): void {
+    if (this.animationItems[category]) {
+      this.animationItems[category].play();
+    }
+  }
+
+  onLeave(category: string): void {
+    if (this.animationItems[category]) {
+      this.animationItems[category].stop();
+    }
+  }
 }
